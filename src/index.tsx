f75fe8af9be998bb7c419441652363bb792872b4
WindrunnerMax <651525974@qq.com>
2023-01-09 09:48:42 +0800

    chore: packages path

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,9 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "src/App.css";
 import { register } from "./utils/components-loader";
-import { image } from "./components/image";
-import { richText } from "./components/text";
-import { blank } from "./components/blank";
+import { image } from "./modules/image";
+import { richText } from "./modules/text";
+import { blank } from "./modules/blank";
 
 register(image, richText, blank);
 ReactDOM.render(<App />, document.getElementById("root"));
